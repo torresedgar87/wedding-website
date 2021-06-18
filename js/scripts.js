53e18af283a401f842000814757f2d70826a9e49
Edgar Torres Roldan <torresedgar@yahoo.com>
2021-06-18 15:18:27 -0700

    fix rsvp functionality

--- js/scripts.js
+++ js/scripts.js
@@ -208,7 +208,7 @@ $(document).ready(function () {
 
 
     /********************** RSVP **********************/
-    $('#rsvp-form').on('submit', function (e) {
+    /*$('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
         var data = $(this).serialize();
 
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbwU4C2INsYyc88mSfgasYMqDnfxYXNoSaVn-ZuPaCOU6KkssIQUdZnQbiXl_N7ZVWMi/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
@@ -232,6 +232,44 @@ $(document).ready(function () {
                     console.log(data);
                     $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
                 });
+        }
+    });*/
+
+    $('#rsvp-form').on('submit', function (e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+
+        $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
+
+        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
+            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
+            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
+        } else {
+            $.ajax({
+                url: "https://script.google.com/macros/s/AKfycbwU4C2INsYyc88mSfgasYMqDnfxYXNoSaVn-ZuPaCOU6KkssIQUdZnQbiXl_N7ZVWMi/exec",
+                type: "POST",
+                contentType: "application/x-www-form-urlencoded", 
+                data: data,
+                
+                complete: function() {
+                    //called when complete
+                },
+                
+                success: function(data) {
+                    console.log(data);
+                    if (data.result === "error") {
+                        $('#alert-wrapper').html(alert_markup('danger', data.message));
+                    } else {
+                        $('#alert-wrapper').html('');
+                        $('#rsvp-modal').modal('show');
+                    }
+                },
+                
+                error: function(data) {
+                    console.log(data);
+                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
+                },
+            });
         }
     });
 
